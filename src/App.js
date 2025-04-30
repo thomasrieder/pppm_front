ba94594e85072c03b0a190f709d13219e6d1abd1
thomasrieder <thom.rieder@gmail.com>
2025-04-30 06:54:51 +0200

    vasy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+//data will be the string we send from our server
+const apiCall = () => {
+  axios.get(BACKEND_URL).then((data) => {
+    //this console.log will be in our frontend console
+    console.log(data)
+  })
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>{BACKEND_URL}</label>
+        <button onClick={apiCall}>Make API Call</button>
       </header>
     </div>
   );
